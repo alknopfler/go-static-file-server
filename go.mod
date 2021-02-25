65e8664f28cb9f7063f4e94345693636dd798b1f
Alberto Morgante <alknopfler@gmail.com>
2021-02-25 17:28:05 +0100

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/alknopfler/go-static-file-server
+
+go 1.15
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+var (
+	dT time.Duration
+
+)
+
+func main() {
+	deadTime := flag.String("t", "10m", "Tiempo de duracion del enlace")
+	dirToShare := flag.String( "d", "./", "Directorio que vamos a compartir")
+	_ = flag.String("h", "", "Options available: -t=10m , -d='/home' ")
+
+	flag.Parse()
+	dT, _ := time.ParseDuration(*deadTime)
+
+	http.Handle("/", http.FileServer(http.Dir(*dirToShare)))
+	log.Println("Starting to share " + *dirToShare + " during " + *deadTime)
+
+	go http.ListenAndServe(":8887", nil)
+
+	select {
+		case <-time.After(dT):
+			log.Println("Run out of time to share...")
+			os.Exit(0)
+	}
+	
+}
